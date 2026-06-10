78b4949c536054cb6256c896ed39f711f0ac6853
GabiiCarvalho <gabriele.carvalho.16@icloud.com>
2026-06-10 15:11:27 -0300

    added random values

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,13 +4,15 @@ import DashboardPage from "../pages/dashboardPage";
 import MenuPage from "../pages/menuPage";
 import MyInfoPage from "../pages/myInfoPage";
 
+const Chance = require("chance");
+const chance = new Chance();
 const loginPage = new LoginPage();
 const dashboardPage = new DashboardPage();
 const menuPage = new MenuPage();
 const myInfoPage = new MyInfoPage();
 
 describe("Orange HRM Tests", () => {
-  it.only("User Info Update - Success", () => {
+  it("User Info Update - Success", () => {
     loginPage.accessLoginPage();
     loginPage.loginWithAnyUser(
       userData.userSuccess.username,
@@ -21,7 +23,11 @@ describe("Orange HRM Tests", () => {
 
     menuPage.accessMyInfo();
 
-    myInfoPage.fillPersonalDetails("Fist Name", "Middle Name", "Last Name");
+    myInfoPage.fillPersonalDetails(
+      chance.first(),
+      chance.name({ middle: true }),
+      chance.last(),
+    );
     myInfoPage.fillEmployeeDetails(
       "EmployeeId",
       "OtherId",
@@ -33,12 +39,5 @@ describe("Orange HRM Tests", () => {
     myInfoPage.saveForm();
 
     //cy.get(selectorsList.genericField).eq(3).clear().type("nathancarvalho"); //employee id
-  });
-  it("Login - Fail", () => {
-    cy.visit("/auth/login");
-    cy.get(selectorsList.usernameField).type(userData.userFail.username);
-    cy.get(selectorsList.passwordField).type(userData.userFail.password);
-    cy.get(selectorsList.loginButton).click();
-    cy.get(selectorsList.wrongCredentialsAlert);
   });
 });
